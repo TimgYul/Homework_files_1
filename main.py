99fce787c55d63b630b5affe9bb9a2f1c47d8b0c
Yuliya Timganova <yulchik12-12y@yandex.ru>
2022-08-12 22:07:30 +0500

    Done

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from pprint import pprint
 from re import L
 
-cook_book = {}
-
 def read_file(f, count):
     i = 0
     
@@ -13,26 +11,49 @@ def read_file(f, count):
         i += 1
         if len(line) == 3:
             book = {}
-            book['ingredient_name'] = line[0]#.strip('\n')
-            book['quantity'] = line[1]#.strip('\n')
-            book['measure'] = line[2]#.strip('\n')
+            book['ingredient_name'] = line[0]
+            book['quantity'] = line[1]
+            book['measure'] = line[2]
             list_book.append(book)
-            #pprint(list_book)
                     
     return list_book
 
+def wish_list(dishes, person_count, cook_book):
+    list_menu = {}
+    for dish in dishes:
+        for list in cook_book[dish]:            
+            ingredient = {}
+            if list['ingredient_name'] in list_menu:
+                temp = {}
+                temp = list_menu[list['ingredient_name']]
+                ingredient['quantity'] = ( int(list['quantity'] ) * person_count) + temp['quantity']
+            else:
+                ingredient['quantity'] = int(list['quantity']) * person_count
+            
+            ingredient['measure'] = list['measure']
+            list_menu[list['ingredient_name']] = ingredient
+    return list_menu
+
+
+
 with open('recipes.txt', encoding='utf-8') as f:
     a = True
+    cook_book = {}
     while a:
         name = f.readline().strip('\n')
-        #print(name)
         if name:
             count = int(f.readline())
             cook_book[name] = read_file(f,count)
         else:
             a = False
 
+    print('\nСостав меню:')
     pprint(cook_book)
+
+    menu = wish_list([ 'Омлет', 'Омлет'], 3 , cook_book)
+    print('\nКоличество ингредиентов:')
+    pprint(menu)
+
 
 
     
